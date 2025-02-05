390224cc8833babb060aa0c77ecebaa482eec61b
Olivia Christensen <oliviapc09@gmail.com>
2025-02-05 14:26:07 -0700

    all done!

--- Mission5Assignment/Controllers/HomeController.cs
+++ Mission5Assignment/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Payment()
+    public IActionResult Calculator()
     {
         return View();
     }
--- Mission5Assignment/wwwroot/js/site.js
+++ Mission5Assignment/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(document).ready(function () {
+    $("#calculateBtn").click(function () {
+        let hours = $("#hours").val(); // get input value for hours
+        let rate = $("#rate").val(); // get hourly rate (set)
+        let total;
+
+        // check to make sure it's a positive input
+        if (hours <= 0 || isNaN(hours)) {
+            alert("Please enter a valid number");
+            $("#total").val(""); // clear total field
+            return;
+        }
+        
+        // calculate total cost
+        total = hours * rate;
+        
+        // display total in output field
+        $("#total").val("$" + total.toFixed(2));
+    })
+})
